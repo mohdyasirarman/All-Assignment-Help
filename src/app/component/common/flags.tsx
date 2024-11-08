954ccc1cfe9393d8ee1db8cc7cb56facbcf29661
Nitin6404 <nc1575612@gmail.com>
2024-11-08 21:02:49 +0530

    flag component is done

--- src/app/component/common/flags.tsx
+++ src/app/component/common/flags.tsx
@@ -1,29 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Flags() {
   const phoneNumbers = [
     {
       country: "US",
-      flag: "/static/images/flags/usa.svg",
+      flag: "/static/images/flags/us.png",
       number: "+1-817-968-5551",
     },
     {
       country: "AU",
-      flag: "/static/images/flags/australia.svg",
+      flag: "/static/images/flags/au.png",
       number: "+61-488-839-671",
     },
     {
-      country: "UK",
-      flag: "/static/images/flags/uk.svg",
+      country: "GB",
+      flag: "/static/images/flags/gb.png",
       number: "+44-7480-542904",
     },
   ];
 
   return (
-    <div className="bg-white border-b border-gray-200">
+    <div className="bg-white border-gray-200">
       <div className="container mx-auto">
-        <div className="flex justify-between items-center h-8">
+        <div className="flex justify-end items-center h-8">
           <div className="flex items-center">
             {phoneNumbers.map((item, index) => (
               <a
@@ -34,8 +35,8 @@ export default function Flags() {
                 <Image
                   src={item.flag}
                   alt={item.country}
-                  width={20}
-                  height={15}
+                  width={16}
+                  height={11}
                   className="rounded-sm mr-2"
                 />
                 <span className="text-[12px] leading-[32px] font-bold text-gray-600">
@@ -49,13 +50,7 @@ export default function Flags() {
               href="/account"
               className="flex items-center mx-[15px] text-[12px] leading-[32px] font-bold text-gray-600 hover:text-blue-600"
             >
-              <Image
-                src="/static/images/icons/user.svg"
-                alt="Account"
-                width={16}
-                height={16}
-                className="mr-2"
-              />
+              <FaUserCircle className="mr-2" height={12} width={12} />
               <span>My Account</span>
             </Link>
           </div>
