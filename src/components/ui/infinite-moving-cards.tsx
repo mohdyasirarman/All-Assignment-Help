7b3c719c13c631d0ea576d9d79c7502feac84864
Nitin6404 <nc1575612@gmail.com>
2024-11-14 10:34:04 +0530

    testimonial animation is done

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -2,6 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import React, { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaStar } from "react-icons/fa";
 
 export const InfiniteMovingCards = ({
   items,
@@ -85,31 +87,44 @@ export const InfiniteMovingCards = ({
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
-        {items.map((item, idx) => (
+        {items.map((item) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
-            style={{
-              background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
-            }}
+            className="w-[250px] h-[240px] max-w-full relative bg-white rounded-xl border-2 flex-shrink-0 border-[#2E2F35] px-8 py-6 md:w-[350px]"
             key={item.name}
           >
             <blockquote>
+              <div className="relative z-20 flex flex-row items-center">
+                <Avatar>
+                  <AvatarImage
+                    src="/static/images/Profile_avatar_placeholder_large.png"
+                    alt="Avatar"
+                    className="w-12 h-12"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <span className="flex flex-col px-3">
+                  <span className="font-inter font-medium text-base leading-[18px] text-[#2E2F35]">
+                    {item.name}
+                  </span>
+                  <span className="font-poppins font-bold text-xs leading-[21px] text-[#2C2C2C] ">
+                    {item.title}
+                  </span>
+                </span>
+                <div className="flex pl-10">
+                  <FaStar color="#F16700" />
+                  <FaStar color="#F16700" />
+                  <FaStar color="#F16700" />
+                  <FaStar color="#F16700" />
+                  <FaStar color="#F16700" />
+                </div>
+              </div>
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
-                {item.quote}
-              </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
-                  </span>
+              <div className="py-5">
+                <span className="relative z-20 text-sm leading-[170%] text-[#2C2C2C] font-normal font-poppins">
+                  {item.quote}
                 </span>
               </div>
             </blockquote>
