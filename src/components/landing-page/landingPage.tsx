b7933a927c458fc20edc02a55704f534f3d220b8
faaiz251 <mohdfaaizazeem@gmail.com>
2024-11-19 03:09:29 +0530

    crated a div

--- src/components/landing-page/landingPage.tsx
+++ src/components/landing-page/landingPage.tsx
@@ -9,6 +9,7 @@ import ComprehensiveHelp from "@/components/landing-page/comprehensiveHelp";
 import AffordablePricing from "@/components/landing-page/affordablePricing";
 import WritingTool from "@/components/landing-page/writingTool";
 import FAQS from "@/components/landing-page/faq";
+import UnlockExpertHelp from "@/components/landing-page/unlockExpertHelp";
 
 export default function LandingPage() {
   return (
@@ -24,6 +25,7 @@ export default function LandingPage() {
       <AffordablePricing />
       <WritingTool />
       <FAQS />
+      <UnlockExpertHelp />
     </div>
   );
 }
---
+++ src/components/landing-page/unlockExpertHelp.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+const unlockExpertHelp = () => {
+  return (
+    <div>
+      <div className="w-full my-20">
+        <div className="flex justify-center px-28 text-lg font-dmSans font-bold">
+          <div className="flex justify-between items-center bg-[#2C2C2C] p-6 rounded-[16px] w-[936px] h-[241px]">
+            {/* Left Section: Text */}
+            <div className="font-poppins  font-bold leading-[60px] text-left decoration-none">
+              <h1 className="text-[50px] font-bold ml-[67px] font-poppins  w-[489.42px] h-[91px] ">
+                <span className="text-[#0DCC5C] ">Unlock</span>{" "}
+                <span className="text-[#FFFFFF]">Expert Help</span> <br />
+                <span className="text-[#FFFFFF]">and Save Big!</span>
+              </h1>
+              <p className="leading-[27px] ml-[67px] mt-[36.6px] font-poppins w-[387px] h-[30px]">
+                <span className="text-[18px] text-[#FFFFFF]">Enjoy</span>{" "}
+                <span className="text-[#0DCC5C] text-[20px]">20% cashback</span>{" "}
+                <span className="text-[20px] text-[#FFFFFF]">
+                  on your first order.
+                </span>
+              </p>
+            </div>
+
+            {/* Right Section: Links and QR Code */}
+            <div className="flex flex-col rounded-lg space-y-4 ml-[0px]">
+              <div className="space-y-4 ">
+                <Image
+                  src="/static/images/apple-bar.svg"
+                  alt="App Store"
+                  width={123}
+                  height={36}
+                  className="mt-[7.5px] "
+                />
+                <Image
+                  src="/static/images/google-bar.svg"
+                  alt="Google play Store"
+                  width={123}
+                  height={36}
+                  className="mt-[111.5.5px] "
+                />
+              </div>
+
+              <div className="w-[259.11px] h-[15px]">
+                <p className="font-poppins font-[400px] text-[10px] text-center text-[#FFFFFF] leading-[10px]">
+                  *Offer eligible for first 3 orders ordered through app!
+                </p>
+              </div>
+            </div>
+            <div className="mt-[-30px]">
+              <Image
+                src="/static/images/qr.svg" // Replace with the actual path to your QR code image
+                alt="QR Code"
+                width={100}
+                height={100}
+                className="flex"
+              />
+              {/* {IMage for qr code} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default unlockExpertHelp;
