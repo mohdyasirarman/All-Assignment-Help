54b0c79cb3566a66bef6e0bf015dc0c2f30367df
Nitin6404 <nc1575612@gmail.com>
2024-12-28 17:05:28 +0530

    deployment issues resolved

--- src/app/services/problem-solving/college-homework-help/page.jsx
+++ src/app/services/problem-solving/college-homework-help/page.jsx
@@ -5,14 +5,13 @@ import UnlockExpertHelp from "@/components/landing-page/unlockExpertHelp";
 import Footer from "@/components/common/footer";
 import AcademicNeeds from "@/components/landing-page/academicneeds";
 import HowItWorks from "@/components/landing-page/howitworks";
-import AffordablePrice from '@/components/landing-page/affordableprice';
+import AffordablePrice from "@/components/landing-page/affordableprice";
 import ThingAboutUs from "@/components/landing-page/thingaboutus";
 import HeroSectionNew from "@/components/landing-page/HeroSectionNew";
 import Experts from "@/components/landing-page/experts";
 import Features from "@/components/landing-page/features";
 
 export default function Page() {
-
   const title = "Need Help with College Homework? ";
   const buttons = [
     "Complete Homework",
@@ -27,145 +26,269 @@ export default function Page() {
   const sections = [
     {
       title: "Tailored Homework Solutions",
-      description: 
+      description:
         "We provide customized homework help that aligns with your specific college assignments and subject requirements.",
       number: "100%",
     },
     {
       title: "Professional Experts",
-      description: 
+      description:
         "Our team comprises experienced experts in various academic disciplines, ready to assist with your college homework.",
       number: "250+",
     },
     {
       title: "Deadline-Oriented",
-      description: 
+      description:
         "We understand the pressure of homework deadlines and ensure timely delivery, so you stay stress-free.",
       number: "98%",
-    }
+    },
   ];
-
 
   const customExperts = [
     {
       name: "Jane Smith",
-      qualification: "Ph.D. in Mathematics",
+      expertise: "Mathematics",
       description: "Expert in advanced calculus and statistical analysis.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2010,
+      orderInProgress: 3,
+      location: "Singapore",
+      rating: "4.9/5",
+      about:
+        "Experienced mathematician specializing in problem-solving and data modeling.",
     },
     {
       name: "Michael Brown",
-      qualification: "Masters in Computer Science",
-      description: "Specialized in AI, machine learning, and big data analytics.",
-      image: "/static/images/headphoneGirl.png",
+      expertise: "Computer Science",
+      description:
+        "Specialized in AI, machine learning, and big data analytics.",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2015,
+      orderInProgress: 4,
+      location: "US",
+      rating: "4.7/5",
+      about:
+        "Innovative computer scientist focusing on artificial intelligence and cutting-edge technology.",
     },
     {
       name: "Emily Davis",
-      qualification: "Masters in Chemistry",
-      description: "Focused on organic synthesis and material science research.",
-      image: "/static/images/headphoneGirl.png",
+      expertise: "Chemistry",
+      description:
+        "Focused on organic synthesis and material science research.",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2012,
+      orderInProgress: 2,
+      location: "UK",
+      rating: "4.6/5",
+      about:
+        "Chemistry expert with a passion for creating novel materials and advancing chemical processes.",
     },
     {
       name: "David Wilson",
-      qualification: "Ph.D. in Economics",
+      expertise: "Economics",
       description: "Expert in macroeconomic policy and financial markets.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2014,
+      orderInProgress: 3,
+      location: "India",
+      rating: "4.8/5",
+      about:
+        "Economist with extensive experience in global financial systems and policy formulation.",
     },
     {
       name: "Sarah Johnson",
-      qualification: "Masters in Biology",
+      expertise: "Biology",
       description: "Specialized in molecular biology and genetic engineering.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2016,
+      orderInProgress: 5,
+      location: "US",
+      rating: "5.0/5",
+      about:
+        "Biologist with expertise in genetic research and advancements in molecular techniques.",
     },
     {
       name: "William Martinez",
-      qualification: "Ph.D. in Environmental Science",
+      expertise: "Environmental Science",
       description: "Focused on climate change modeling and sustainability.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2011,
+      orderInProgress: 2,
+      location: "Canada",
+      rating: "4.7/5",
+      about:
+        "Environmental scientist dedicated to finding sustainable solutions for climate challenges.",
     },
     {
       name: "Olivia Taylor",
-      qualification: "Masters in Psychology",
+      expertise: "Psychology",
       description: "Expert in cognitive behavioral therapy and mental health.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2013,
+      orderInProgress: 4,
+      location: "UK",
+      rating: "4.9/5",
+      about:
+        "Psychologist specializing in therapeutic approaches for mental wellness and personal growth.",
     },
     {
       name: "Alexander Moore",
-      qualification: "Ph.D. in Astrophysics",
-      description: "Researcher in black holes, dark matter, and the origins of the universe.",
-      image: "/static/images/headphoneGirl.png",
+      expertise: "Astrophysics",
+      description:
+        "Researcher in black holes, dark matter, and the origins of the universe.",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2010,
+      orderInProgress: 1,
+      location: "US",
+      rating: "4.6/5",
+      about:
+        "Astrophysicist with a deep focus on space phenomena and the mysteries of the cosmos.",
     },
     {
       name: "Sophia Anderson",
-      qualification: "Masters in Education",
-      description: "Specialized in curriculum development and learning strategies.",
-      image: "/static/images/headphoneGirl.png",
+      expertise: "Education",
+      description:
+        "Specialized in curriculum development and learning strategies.",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2014,
+      orderInProgress: 2,
+      location: "Australia",
+      rating: "4.8/5",
+      about:
+        "Education expert designing innovative teaching methods to enhance learning outcomes.",
     },
     {
       name: "Ethan Carter",
-      qualification: "Ph.D. in Philosophy",
+      expertise: "Philosophy",
       description: "Expert in ethics, logic, and existential theories.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2015,
+      orderInProgress: 1,
+      location: "US",
+      rating: "4.9/5",
+      about:
+        "Philosopher passionate about exploring the depths of human thought and moral dilemmas.",
     },
     {
       name: "Isabella Lee",
-      qualification: "Masters in Literature",
-      description: "Focused on contemporary literary analysis and creative writing.",
-      image: "/static/images/headphoneGirl.png",
+      expertise: "Literature",
+      description:
+        "Focused on contemporary literary analysis and creative writing.",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2013,
+      orderInProgress: 3,
+      location: "UK",
+      rating: "4.7/5",
+      about:
+        "Literature expert with a love for analyzing modern texts and fostering creative expression.",
     },
     {
       name: "Lucas Harris",
-      qualification: "Masters in Engineering",
+      expertise: "Engineering",
       description: "Specialized in robotics and automation systems.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2012,
+      orderInProgress: 2,
+      location: "Germany",
+      rating: "4.8/5",
+      about:
+        "Engineer focused on developing intelligent machines and enhancing automation across industries.",
     },
     {
       name: "Amelia Scott",
-      qualification: "Ph.D. in Sociology",
+      expertise: "Sociology",
       description: "Expert in urban sociology and social behavior analysis.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2011,
+      orderInProgress: 3,
+      location: "Canada",
+      rating: "4.6/5",
+      about:
+        "Sociologist with a keen interest in understanding social structures and urban dynamics.",
     },
     {
       name: "Benjamin Evans",
-      qualification: "Masters in Political Science",
+      expertise: "Political Science",
       description: "Focused on international relations and public policy.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2014,
+      orderInProgress: 4,
+      location: "US",
+      rating: "4.7/5",
+      about:
+        "Political scientist analyzing global trends and shaping policies for international cooperation.",
     },
     {
       name: "Charlotte Thompson",
-      qualification: "Ph.D. in Art History",
+      expertise: "Art History",
       description: "Specialized in Renaissance and modern art movements.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2016,
+      orderInProgress: 2,
+      location: "Italy",
+      rating: "5.0/5",
+      about:
+        "Art historian with a deep appreciation for cultural evolution and iconic art periods.",
     },
     {
       name: "Daniel King",
-      qualification: "Masters in Law",
+      expertise: "Law",
       description: "Expert in corporate law and intellectual property.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2010,
+      orderInProgress: 3,
+      location: "US",
+      rating: "4.9/5",
+      about:
+        "Corporate lawyer with a specialization in intellectual property rights and business law.",
     },
     {
       name: "Grace White",
-      qualification: "Ph.D. in Archaeology",
+      expertise: "Archaeology",
       description: "Researcher in ancient civilizations and cultural heritage.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2012,
+      orderInProgress: 1,
+      location: "Egypt",
+      rating: "4.8/5",
+      about:
+        "Archaeologist committed to preserving ancient history and discovering lost civilizations.",
     },
     {
       name: "Liam Rodriguez",
-      qualification: "Masters in Marketing",
+      expertise: "Marketing",
       description: "Specialized in digital marketing and brand strategy.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2015,
+      orderInProgress: 3,
+      location: "Brazil",
+      rating: "4.7/5",
+      about:
+        "Marketing expert helping brands enhance their presence through strategic digital initiatives.",
     },
     {
       name: "Chloe Walker",
-      qualification: "Masters in Nutrition",
+      expertise: "Nutrition",
       description: "Focused on dietetics and sports nutrition.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2013,
+      orderInProgress: 2,
+      location: "US",
+      rating: "4.9/5",
+      about:
+        "Nutritionist dedicated to optimizing health through personalized diet and nutrition plans.",
     },
     {
       name: "Noah Phillips",
-      qualification: "Ph.D. in History",
+      expertise: "History",
       description: "Expert in medieval history and historical preservation.",
-      image: "/static/images/headphoneGirl.png",
+      image: "/static/images/Profile_avatar_placeholder_large.png",
+      orderFinished: 2011,
+      orderInProgress: 3,
+      location: "UK",
+      rating: "4.8/5",
+      about:
+        "Historian specializing in medieval periods and the conservation of cultural landmarks.",
     },
   ];
 
@@ -209,15 +332,17 @@ export default function Page() {
       question: "How do I get started?",
       answer:
         "Getting started is simple. Just share your homework requirements and deadlines, and we’ll handle the rest.",
-    }
+    },
   ];
 
   return (
     <>
-      <HeroSectionNew 
-        changeInH={"Get Expert Help with College Homework"} 
-        changeInP={"Receive professional assistance to complete college homework successfully."} 
-        isboy={false} 
+      <HeroSectionNew
+        changeInH={"Get Expert Help with College Homework"}
+        changeInP={
+          "Receive professional assistance to complete college homework successfully."
+        }
+        isboy={false}
       />
       <AcademicNeeds title={title} buttons={buttons} />
       <WhyStudentTrustUs sections={sections} />
@@ -225,14 +350,16 @@ export default function Page() {
       <ThingAboutUs />
       <Features />
       <HowItWorks />
-      <WhatStudentSayAboutUs 
-        changeInP={"Hear from students who successfully completed their homework with our help."} 
-        changeTitle={"What Our Clients Say About Us"} 
+      <WhatStudentSayAboutUs
+        changeInP={
+          "Hear from students who successfully completed their homework with our help."
+        }
+        changeTitle={"What Our Clients Say About Us"}
       />
       <AffordablePrice />
       <FAQS questions={questions} />
       <UnlockExpertHelp />
-      <Footer />          
+      <Footer />
     </>
   );
 }
